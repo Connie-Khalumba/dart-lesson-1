2be15ca06999518d200610aff0489caa4fb756ab
constance khalumba <constancekhalumba@gmail.com>
2024-10-15 10:14:52 +0300

    control flow statements

---
+++ if-else-if.dart
@@ -0,0 +1,21 @@
+void main() {
+  int marks = 5;
+
+  if (marks > 80) {
+    print("🗳️ Excellent score keep it up");
+  } else if (marks == 70) {
+    print("🎉 Very good!");
+  } else if (marks == 60){
+    print("👧 Above average score!");
+  } else if (marks == 50){
+    print("👧 Average try and Improve! ");
+}  else if (marks == 40){
+    print("👧 Below average!");
+} else if (marks == 30){
+    print("Poor score!");
+ }  else if (marks == 20){
+    print("I need to see your parent!"); 
+    } else {
+    print("���‍��️ Failed! You need prayers");
+  }
+}
---
+++ switch-statement.dart
@@ -0,0 +1,24 @@
+void main() {
+  int score = 80;
+
+  if (score >= 90) {
+    print("🌞 You scored excellent marks!");
+  } else if (score >= 80) {
+    print("🚀 Great work! Keep it up!");
+  } else if (score >= 70) {
+    print("📘 Good effort, but there's room for improvement.");
+  } else if (score >= 60) {
+    print("🔄 Fair, but you can do better.");
+  } else if (score >= 50) {
+    print("⚠️ You passed, but it's a close call.");
+  } else if (score >= 40) {
+    print("⚠️ You can work more harder, its indeed a close call");
+  } else if (score >= 30) {
+    print("⚠️ You failed terribly");
+  } else if (score >= 20) {
+    print("⚠️ Bring your guardian Tomorrow first thing in the morning!.");
+  } else {
+    print("❌ You need to work harder next time.");
+  }
+}
+
